21ca818c8f4fe2d8ed393cd7fb6f7ace987cac45
hamidr-source <hr454837@gmail.com>
2024-09-23 21:52:55 +0330

    Refactor the id generate for todos

--- src/components/context/Context.jsx
+++ src/components/context/Context.jsx
@@ -1,18 +1,19 @@
 import React, { createContext, useState } from "react";
-import { v4 } from "uuid";
 
 export const TodoContext = createContext();
 
 export const todoData = {
-  todos: [{ id: 1, description: "" }],
+  todos: [],
 };
 
 const TodoProvider = ({ children }) => {
   const [todo, setTodo] = useState(todoData.todos);
 
+
   function handleAddTodo(description) {
-    setTodo([...todo, { id: v4(), description: description }]);
+    setTodo([...todo, { id: todo.length + 1, description: description }]);
   }
+
   return (
     <TodoContext.Provider value={{ todo, handleAddTodo }}>
       {children}
